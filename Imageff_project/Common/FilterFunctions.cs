7910f8004712402f0d76e2ef08c58141927892e8
Konstantin Todorov <kz.todorov@gmail.com>
2012-12-17 18:37:21 +0200

    Initialized.

---
+++ Imageff_project/Common/FilterFunctions.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI;
+
+namespace Imageff_project.Common
+{
+	class FilterFunctions
+	{
+		static byte[] invertedAlreadyArray, blackWhiteAlreadyArray;
+
+		public static void BlackAndWhite(ref byte[] dstPixels, ref byte[] srcPixels, ref int height, ref int width, bool alreadyDone = false)
+		{
+			int currentByte = 0;
+			if (alreadyDone)
+			{
+				dstPixels = blackWhiteAlreadyArray;
+			}
+			else
+			{
+				while (currentByte < (4 * height * width))
+				{
+					blackWhiteAlreadyArray = srcPixels;
+					int baw = (srcPixels[currentByte] + srcPixels[currentByte + 1] + srcPixels[currentByte + 2]) / 3;
+					Color tempColor = Color.FromArgb(srcPixels[currentByte + 3], (byte)baw, (byte)baw, (byte)baw);
+					dstPixels[currentByte++] = tempColor.B;
+					dstPixels[currentByte++] = tempColor.G;
+					dstPixels[currentByte++] = tempColor.R;
+					dstPixels[currentByte++] = tempColor.A;
+
+				}
+			}
+		}
+
+		public static void Filter(ref byte[] dstPixels, ref byte[] srcPixels, ref int height, ref int width, bool invertedAlready = false)
+		{
+			int currentByte = 0;
+			if (invertedAlready)
+			{
+				dstPixels = invertedAlreadyArray;
+				invertedAlreadyArray = new byte[0];
+			}
+			else
+			{
+				while (currentByte < (4 * height * width))
+				{
+					invertedAlreadyArray = srcPixels;
+					dstPixels[currentByte++] -= 255;
+					dstPixels[currentByte++] -= 255;
+					dstPixels[currentByte++] -= 255;
+					dstPixels[currentByte++] = srcPixels[currentByte - 3];
+				}
+
+			}
+		}
+
+		public static void Darken(ref byte[] dstPixels, ref byte[] srcPixels, ref int height, ref int width, double value)
+		{
+			double darkness = -value;
+			darkness = (1 / darkness);
+			if (darkness != 1)
+				darkness += .1;
+			int currentByte = 0;
+			while (currentByte < (4 * height * width))
+			{
+				dstPixels[currentByte] = (byte)(srcPixels[currentByte++] * darkness);
+				dstPixels[currentByte] = (byte)(srcPixels[currentByte++] * darkness);
+				dstPixels[currentByte] = (byte)(srcPixels[currentByte++] * darkness);
+				dstPixels[currentByte] = srcPixels[currentByte++];
+			}
+		}
+
+		public static void Lighten(ref byte[] dstPixels, ref byte[] srcPixels, ref int height, ref int width, double value)
+		{
+			double brightness = (value * 0.05) + 1;
+			int currentByte = 0;
+			while (currentByte < (4 * height * width))
+			{
+				if ((srcPixels[currentByte] * brightness) > 255)
+					dstPixels[currentByte++] = 255;
+				else
+					dstPixels[currentByte] = (byte)(srcPixels[currentByte++] * brightness);
+				if ((srcPixels[currentByte] * brightness) > 255)
+					dstPixels[currentByte++] = 255;
+				else
+					dstPixels[currentByte] = (byte)(srcPixels[currentByte++] * brightness);
+				if ((srcPixels[currentByte] * brightness) > 255)
+					dstPixels[currentByte++] = 255;
+				else
+					dstPixels[currentByte] = (byte)(srcPixels[currentByte++] * brightness);
+				dstPixels[currentByte] = srcPixels[currentByte++];
+			}
+		}
+
+	}
+}
---
+++ Imageff_project/MainPage.xaml.cs
@@ -0,0 +1,252 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.UI.Xaml;
+using Windows.UI;
+using Windows.Graphics.Imaging;
+using Windows.Storage.Streams;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Media;
+using Windows.Storage;
+using Windows.Storage.Pickers;
+using Windows.Storage.Provider;
+using Imageff_project.Common;
+
+
+#region Namespace Imageff_project
+namespace Imageff_project
+{
+	#region MainPage class
+	public sealed partial class MainPage : Imageff_project.Common.LayoutAwarePage
+	{
+		private string mruToken = null;
+		private WriteableBitmap tempBitmap;
+		byte[] srcPixels, dstPixels;
+		int width, height;
+		Stream temp;
+		bool invertedAlready = false, blackWhiteAlready = false;
+		static readonly long cycleDuration = TimeSpan.FromSeconds(3).Ticks;
+		bool pictureIsLoaded = false;
+		Windows.Storage.StorageFile Globalfile;
+
+
+		public MainPage()
+		{
+			this.InitializeComponent();
+		}
+
+		#region This is the LoadState.
+		protected async override void LoadState(Object navigationParameter, Dictionary<String, Object> pageState)
+		{
+			if (pageState != null && pageState.ContainsKey("mruToken"))
+			{
+				object value = null;
+				if (pageState.TryGetValue("mruToken", out value))
+				{
+					if (value != null)
+					{
+						mruToken = value.ToString();
+
+						// Open the file via the token that you stored when adding this file into the MRU list.
+						Windows.Storage.StorageFile file =
+							await Windows.Storage.AccessCache.StorageApplicationPermissions.MostRecentlyUsedList.GetFileAsync(mruToken);
+
+						if (file != null)
+						{
+							// Open a stream for the selected file.
+							Windows.Storage.Streams.IRandomAccessStream fileStream =
+								await file.OpenAsync(Windows.Storage.FileAccessMode.Read);
+							tempBitmap.SetSource(fileStream);
+							displayImage.Source = tempBitmap;
+
+							// Set the data context for the page.
+							this.DataContext = file;
+						}
+					}
+				}
+			}
+		}
+		#endregion
+
+		#region This is the SaveState.
+		protected override void SaveState(Dictionary<String, Object> pageState)
+		{
+			if (!String.IsNullOrEmpty(mruToken))
+			{
+				pageState["mruToken"] = mruToken;
+			}
+		}
+		#endregion
+
+		#region GetPhotoButton_Click function
+		// This occures when GetPhoto button is clicked
+		private async void GetPhotoButton_Click(object sender, RoutedEventArgs e)
+		{
+			// File picker APIs don't work if the app is in a snapped state.
+			// If the app is snapped, try to unsnap it first. Only show the picker if it unsnaps.
+			if (Windows.UI.ViewManagement.ApplicationView.Value != Windows.UI.ViewManagement.ApplicationViewState.Snapped ||
+				 Windows.UI.ViewManagement.ApplicationView.TryUnsnap() == true)
+			{
+				Windows.Storage.Pickers.FileOpenPicker openPicker = new Windows.Storage.Pickers.FileOpenPicker();
+				openPicker.SuggestedStartLocation = Windows.Storage.Pickers.PickerLocationId.PicturesLibrary;
+				openPicker.ViewMode = Windows.Storage.Pickers.PickerViewMode.Thumbnail;
+
+				// Filter to include a sample subset of file types.
+				openPicker.FileTypeFilter.Clear();
+				openPicker.FileTypeFilter.Add(".bmp");
+				openPicker.FileTypeFilter.Add(".png");
+				openPicker.FileTypeFilter.Add(".jpeg");
+				openPicker.FileTypeFilter.Add(".jpg");
+
+				// Open the file picker.
+				Windows.Storage.StorageFile file = await openPicker.PickSingleFileAsync();
+				Globalfile = file;
+
+				// file is null if user cancels the file picker.
+				if (file != null)
+				{
+					// Open a stream for the selected file.
+					Windows.Storage.Streams.IRandomAccessStream fileStream =
+						await file.OpenAsync(Windows.Storage.FileAccessMode.Read);
+
+					RandomAccessStreamReference streamRef = RandomAccessStreamReference.CreateFromFile(file);
+
+					using (IRandomAccessStreamWithContentType tempStream =
+						   await streamRef.OpenReadAsync())
+					{
+						BitmapDecoder decoder = await BitmapDecoder.CreateAsync(tempStream);
+						BitmapFrame frame = await decoder.GetFrameAsync(0);
+
+						// I know the parameterless version of GetPixelDataAsync works for this image
+						PixelDataProvider pixelProvider = await frame.GetPixelDataAsync();
+						srcPixels = pixelProvider.DetachPixelData();
+
+						tempBitmap = new WriteableBitmap((int)frame.PixelWidth, (int)frame.PixelHeight);
+					}
+					tempBitmap.SetSource(fileStream);
+					// apply pixels to bitmap
+					displayImage.Source = tempBitmap;
+					setFileProperties(file);
+					brightSlider.Value = 0;
+					contentGrid.Opacity = 100;
+					pictureIsLoaded = true;
+					border.BorderThickness = new Thickness(1,1,1,1);
+					border.BorderBrush = new Windows.UI.Xaml.Media.SolidColorBrush(Windows.UI.Colors.Black);
+				}
+			}
+		}
+		#endregion
+
+		#region setFileProperties function
+		void setFileProperties(Windows.Storage.StorageFile file)
+		{
+			// This sets the file name, path and date created
+			// to the text boxes
+			fileName.Text = file.DisplayName;
+		}
+		#endregion
+
+		#region OnInvertClick event
+		private void OnInvertClick(object sender, RoutedEventArgs e)
+		{
+			// This occures when InvertFilterButton is clicked
+			if (pictureIsLoaded)
+			{
+				prepareImage();
+				FilterFunctions.Filter(ref dstPixels, ref srcPixels, ref height, ref width, invertedAlready);
+				invertedAlready = !invertedAlready;
+				setStream();
+			}
+		}
+		#endregion
+
+		#region OnBlackWhiteClick event
+		private void OnBlackWhiteClick(object sender, RoutedEventArgs e)
+		{
+			// This occures when OnBlackWhiteButton is clicked
+			if (pictureIsLoaded)
+			{
+				prepareImage();
+				FilterFunctions.BlackAndWhite(ref dstPixels, ref srcPixels, ref height, ref width, blackWhiteAlready);
+				blackWhiteAlready = !blackWhiteAlready;
+				setStream();
+			}
+		}
+		#endregion
+
+		#region setStream function
+		void setStream()
+		{
+			// This sets the pixels to the bitmap
+			temp.Seek(0, SeekOrigin.Begin);
+			temp.Write(dstPixels, 0, dstPixels.Length);
+			tempBitmap.Invalidate();
+		}
+		#endregion
+
+		#region prepareImage function
+		void prepareImage()
+		{
+			// This calculates the width and height of the bitmap image
+			// and sets the Stream and the pixels byte array
+			width = tempBitmap.PixelWidth;
+			height = tempBitmap.PixelHeight;
+			temp = tempBitmap.PixelBuffer.AsStream();
+			dstPixels = new byte[4 * tempBitmap.PixelWidth * tempBitmap.PixelHeight];
+		}
+		#endregion
+
+		private async void OnSaveButtonClick(object sender, RoutedEventArgs e)
+		{
+			if (pictureIsLoaded)
+			{
+				FileSavePicker savePicker = new FileSavePicker();
+				savePicker.SuggestedStartLocation = PickerLocationId.DocumentsLibrary;
+				// Dropdown of file types the user can save the file as
+				savePicker.FileTypeChoices.Add("Plain Text", new List<string>() { ".bmp" });
+				// Default file name if the user does not type one in or select a file to replace
+				savePicker.SuggestedFileName = fileName.Text;
+				StorageFile file = await savePicker.PickSaveFileAsync();
+
+				if (file != null)
+				{
+					//Windows.Storage.Streams.IRandomAccessStream fileStream =
+					IRandomAccessStream writeStream = await file.OpenAsync(FileAccessMode.ReadWrite);
+					BitmapEncoder encoder = await BitmapEncoder.CreateAsync(BitmapEncoder.BmpEncoderId, writeStream);
+					encoder.SetPixelData(BitmapPixelFormat.Rgba8, BitmapAlphaMode.Premultiplied, (uint)tempBitmap.PixelWidth, (uint)tempBitmap.PixelHeight, tempBitmap.PixelWidth, tempBitmap.PixelHeight, dstPixels);
+					await encoder.FlushAsync();
+					//writeStream.GetOutputStreamAt(0).FlushAsync().Start();
+					debugText.Text = "SUCCESFUL";
+				}
+			}
+		}
+
+		
+
+		private void OnSizeChanged(object sender, Windows.UI.Xaml.Controls.Primitives.RangeBaseValueChangedEventArgs e)
+		{
+			if (pictureIsLoaded)
+			{
+				prepareImage();
+				if (brightSlider.Value < 0)
+				{
+					FilterFunctions.Darken(ref dstPixels, ref srcPixels, ref height, ref width, brightSlider.Value);
+				}
+				else if (brightSlider.Value >= 0)
+				{
+					FilterFunctions.Lighten(ref dstPixels, ref srcPixels, ref height, ref width, brightSlider.Value);
+				}
+				setStream();
+			}
+		}
+		private void BlackAndWhiteButton_PointerEntered_1(object sender, Windows.UI.Xaml.Input.PointerRoutedEventArgs e)
+		{
+			//BlackAndWhiteButton.Foreground = new SolidColorBrush(Colors.Yellow);
+		}
+
+		
+	}
+	#endregion
+}
+#endregion
