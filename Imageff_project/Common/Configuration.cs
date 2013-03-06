264bd06a89415ec14d3c42f1f192db7495b46aef
Anton94 <antonvdudov@gmail.com>
2013-03-06 14:40:59 +0200

    export

--- Imageff_project/Common/Configuration.cs
+++ Imageff_project/Common/Configuration.cs
@@ -5,15 +5,16 @@ using System.Text;
 using System.Threading.Tasks;
 using Windows;
 using Windows.Storage;
+using Windows.Storage.Pickers;
+
 
 namespace RemedyPic.Common
 {
 	class Configuration
-	{
-		public StorageFile configFile
+    {
+        #region Getters and setters.
+        public StorageFile configFile
 		{
-
-			#region Getters and setters.
 			get
 			{
 				return configFile;
@@ -35,8 +36,26 @@ namespace RemedyPic.Common
 				effects = value;
 			}
 		}
-			#endregion
+		#endregion
 
+        public async void Export()
+        {
+            FileSavePicker savePicker = new FileSavePicker();
+
+            // Set My Documents folder as suggested location if no past selected folder is available
+            savePicker.SuggestedStartLocation = PickerLocationId.DocumentsLibrary;
+
+            // Dropdown of file types the user can save the file as
+            savePicker.FileTypeChoices.Add(".TXT", new List<string>() { ".txt" });
+
+            // Default file name if the user does not type one in or select a file to replace
+
+            StorageFile file = await savePicker.PickSaveFileAsync();
+
+            
+
+
+        }
 
 
 	}
