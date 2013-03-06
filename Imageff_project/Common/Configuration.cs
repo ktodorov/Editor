912868cfa00037255af9c39d1f5a5c73d066920f
Konstantin Todorov <kz.todorov@gmail.com>
2013-03-06 14:06:44 +0200

    Added Configuration class.

---
+++ Imageff_project/Common/Configuration.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows;
+using Windows.Storage;
+
+namespace RemedyPic.Common
+{
+	class Configuration
+	{
+		public StorageFile configFile
+		{
+
+			#region Getters and setters.
+			get
+			{
+				return configFile;
+			}
+			set
+			{
+				configFile = value;
+			}
+		}
+
+		public string[] effects
+		{
+			get
+			{
+				return effects;
+			}
+			set
+			{
+				effects = value;
+			}
+		}
+			#endregion
+
+
+
+	}
+}
